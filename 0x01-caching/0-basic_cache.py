784222810a944f4c6444445e34f0af79e0444825
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-29 19:58:01 +0300

    Implement a basic cache as a key value pair

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Basic cache class."""
+
+
+class BasicCache(BaseCaching):
+    """Basic cache."""
+
+    def __init__(self):
+        """Inherit init from parent class."""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add cache as a key value pair."""
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get value associaated to ak key."""
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
